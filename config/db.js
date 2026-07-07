92ae9a44d958f2d6db00d542275982ea5c74df6d
unknown <erlgimpaya3@gmail.com>
2026-07-07 22:48:11 +0800

    Commit sample from my pc branch

--- config/db.js
+++ config/db.js
@@ -11,12 +11,12 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0
 });
-console.log({
-  DB_HOST: process.env.DB_HOST,
-  DB_PORT: process.env.DB_PORT,
-  DB_USER: process.env.DB_USER,
-  DB_NAME: process.env.DB_NAME
-});
+// console.log({
+//   DB_HOST: process.env.DB_HOST,
+//   DB_PORT: process.env.DB_PORT,
+//   DB_USER: process.env.DB_USER,
+//   DB_NAME: process.env.DB_NAME
+// });
 async function initDatabase() {
   const connection = await pool.getConnection();
   try {
